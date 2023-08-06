37cd6fb7c83f8f283bc4ac7acd63ec2cf2ffc900
riteshgeeky <ritesh.benjwal@geeky.dev>
2023-08-06 11:07:38 +0530

    Side bar ui and get chats part done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ChatProvider>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ChatProvider>
 );
